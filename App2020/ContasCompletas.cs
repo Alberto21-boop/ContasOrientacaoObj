35213af7490948f90e4ad00493fe7789d5c807de
alberto <albertobarbosa0003@gmail.com>
2022-04-26 09:44:01 -0300

    Adicionar arquivos de projeto.

---
+++ App2020/ContasCompletas.cs
@@ -0,0 +1,13 @@
+﻿using App2020;
+
+Contas contas = new Contas();
+
+contas.Soma = int.Parse(Console.ReadLine());
+contas.Subtracao = int.Parse(Console.ReadLine());
+contas.Mutiplicacao = double.Parse(Console.ReadLine());
+contas.Divicao = double.Parse(Console.ReadLine());
+
+Console.WriteLine($"Soma : {contas.Somar()}");
+Console.WriteLine($"Subtração : {contas.Subitrair()}");
+Console.WriteLine($"Mutiplicação : {contas.Multiplicar()}");
+Console.WriteLine($"Divição : {contas.Dividir()}");
---
+++ App2020/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace App2020
+{
+    internal class Contas
+    {
+        public int Soma { get; set; }
+        public int Subtracao { get; set; }
+        public double Mutiplicacao { get; set; }
+        public double Divicao { get; set; }
+
+        public int Somar()
+        {
+            return Soma + Subtracao;
+        }
+        public int Subitrair()
+        {
+            return Subtracao - Soma;
+        }
+        public double Multiplicar()
+        {
+            return Mutiplicacao * Soma;
+        }
+        public double Dividir()
+        {
+            return Divicao / Mutiplicacao;
+        }
+    }
+}
